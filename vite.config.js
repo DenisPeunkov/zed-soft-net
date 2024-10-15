ddb2abe2248e700448a8fb1b59a48c316fe49317
Denis <peunkovdv>
2024-10-15 22:33:40 +0700

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/zero-soft-net/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/zed-soft-net/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
